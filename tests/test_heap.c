5c5ef9086931e375fd2e233aed5757b50af8d505
Roberto Fernandes Fortes Neto <202219060605@lab1-l.facom.ufms.br>
2023-03-28 16:26:57 -0400

    melhorando test

--- tests/test_heap.c
+++ tests/test_heap.c
@@ -1,14 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "../src/facom.h"
-#include <assert.h>
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include"../src/facom.h"
+#include<assert.h>
+
 
 void test_pai(){
-	assert(heap_pai(0) == 0);
-	assert(heap_pai(1) == 0);
+    assert(heap_pai(0)==0);
+    assert(heap_pai(1)==0);
+    assert(heap_pai(2)==0);
+    assert(heap_pai(3)==1);
+    assert(heap_pai(4)==1);
+    assert(heap_pai(5)==2);
+    assert(heap_pai(6)==2);
+    assert(heap_pai(7)==3);
+    assert(heap_pai(8)==3);
 }
 
+
 int main(void){
-	test_pai();
-	return EXIT_SUCCESS;
+    test_pai();
+    return EXIT_SUCCESS;
 }
